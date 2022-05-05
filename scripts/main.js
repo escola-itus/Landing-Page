3a1b826175e033072e3737712d9fa5480f8c3cc7
davidson <davidsonmarra@gmail.com>
2022-05-05 12:29:18 -0300

    add feedbacks

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,7 @@
 import header from './modules/header.js';
 import mobileNavbar from './modules/nav.js';
 import './modules/modal.js';
+import './modules/swiper.js';
 
 header.init();
 mobileNavbar.init();
---
+++ scripts/modules/swiper.js
@@ -0,0 +1,30 @@
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js';
+
+const body = document.getElementsByTagName("BODY")[0];
+let slidesPerView = body.offsetWidth > 550 ? 2 : 1
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets'
+  },
+  slidesPerView,
+  spaceBetween: 30,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
+  },
+});
