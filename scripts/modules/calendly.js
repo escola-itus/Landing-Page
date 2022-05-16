114c316dfe4210d2e3bbc68030388d42e00d9773
davidson <davidsonmarra@gmail.com>
2022-05-16 11:55:52 -0300

    add footer

--- scripts/modules/calendly.js
+++ scripts/modules/calendly.js
@@ -1,7 +1,7 @@
 const calendly = document.querySelectorAll('.calendly');
 calendly.forEach(element => element.addEventListener('click', () => {
   Calendly.initPopupWidget({
-    url: 'https://calendly.com/davidsonmarra/itus?hide_event_type_details=1&hide_gdpr_banner=1&text_color=07224b&primary_color=269912'}
+    url: 'https://calendly.com/itus_escola/aula-experimental?hide_event_type_details=1&hide_gdpr_banner=1&primary_color=3f9a15'}
   );
   return false;
 }))
